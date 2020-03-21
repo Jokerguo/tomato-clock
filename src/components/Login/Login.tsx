f09974f39b9c491efc63554ac2c0505722feb857
jokerguo <1977036313@qq.com>
2020-03-21 12:54:36 +0800

    封装onChange

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import {Button, Input} from 'antd';
 import {UserOutlined} from '@ant-design/icons';
-import axios from '../../config/axios'
-import { Link } from 'react-router-dom';
-import './Login.scss'
+import axios from '../../config/axios';
+import {Link} from 'react-router-dom';
+import './Login.scss';
 
 interface LoginState {
   account: string,
@@ -19,12 +19,11 @@ class Login extends React.Component<any, LoginState> {
     };
   }
 
-  onChangeAccount = (e: any) => {
-    this.setState({account: e.target.value});
-  };
 
-  onChangePassword = (e: any) => {
-    this.setState({password: e.target.value});
+  onChange = (key: keyof LoginState, value: string) => {
+    const newState = {} as any;
+    newState[key] = value  ;
+    this.setState(newState);
   };
 
   submit = async () => {
@@ -35,7 +34,7 @@ class Login extends React.Component<any, LoginState> {
         password,
       });
       console.log(this.props);
-      this.props.history.push('/')
+      this.props.history.push('/');
     } catch (e) {
       throw new Error(e);
     }
@@ -47,11 +46,11 @@ class Login extends React.Component<any, LoginState> {
       <div className='Login'>
         <h1>登录番茄</h1>
         <Input placeholder="请输入用户名" prefix={<UserOutlined/>}
-               value={account} onChange={this.onChangeAccount}/>
+               value={account} onChange={e => this.onChange('account', e.target.value)}/>
         <Input.Password placeholder="请输入密码" value={password}
-                        onChange={this.onChangePassword}/>
+                        onChange={e => this.onChange('password', e.target.value)}/>
         <Button className='button' onClick={this.submit}>登录</Button>
-        <p>如果未注册用户,请立即<Link to={`/signUp`} >注册</Link></p>
+        <p>如果未注册用户,请立即<Link to={`/signUp`}>注册</Link></p>
       </div>
     );
   }
