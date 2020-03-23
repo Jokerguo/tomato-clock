f7edd5be47c4fdd702f1723d37051bcb82d804df
jokerguo <1977036313@qq.com>
2020-03-23 13:06:25 +0800

    添加addTodo 到 TodoInput组件

--- src/components/Todos/TodoInput.tsx
+++ src/components/Todos/TodoInput.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {addTodo} from '../../redux/actions';
 import {Input} from 'antd';
 import {EnterOutlined} from '@ant-design/icons';
+import axios from '../../config/axios'
 
 interface TodoInputState {
   description: string
 }
+interface ITodoInputProps {
+  addTodo: (payload:any) => any;
+}
 
-class TodoInput extends React.Component<any, TodoInputState> {
+class TodoInput extends React.Component<ITodoInputProps,TodoInputState> {
   constructor(props: any) {
     super(props);
     this.state = {
@@ -14,20 +20,25 @@ class TodoInput extends React.Component<any, TodoInputState> {
     };
   }
 
-  addTodo = () => {
-      this.props.addTodo({description: this.state.description});
+  postTodo = async () => {
+    try {
+      let response = await axios.post('todos',{description: this.state.description});
+      this.props.addTodo(response.data.resource)
+    }catch (e) {
+      throw new Error(e)
+    }
       this.setState({description: ''});
   };
 
-  onKeyUp = (e: any) => {
+  onKeyUp = async (e: any) => {
     if (e.keyCode === 13 && this.state.description !== '') {
-      this.addTodo();
+      await this.postTodo();
     }
   };
 
   render() {
     const {description} = this.state;
-    const suffix = description ? <EnterOutlined onClick={() => this.addTodo()}/> : <span/>;
+    const suffix = description ? <EnterOutlined onClick={() => this.postTodo()}/> : <span/>;
     return (
       <div >
         <Input placeholder="添加任务" suffix={suffix} value={description}
@@ -37,5 +48,13 @@ class TodoInput extends React.Component<any, TodoInputState> {
     );
   }
 }
+const mapStateToProps = (state: any, ownProps: any) => {
+  return {
+    ...ownProps
+  };
+};
+const mapDispatchToProps = {
+  addTodo
+};
 
-export default TodoInput;
+export default connect(mapStateToProps, mapDispatchToProps)(TodoInput);
